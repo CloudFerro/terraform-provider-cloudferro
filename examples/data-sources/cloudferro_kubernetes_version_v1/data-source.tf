28a6cab8da695c475539242cd3433e8276202261
Szymon Rams <srams@cloudferro.com>
2025-06-10 11:57:36 +0200

    remove data sources

--- examples/data-sources/cloudferro_kubernetes_version_v1/data-source.tf
+++
@@ -1,8 +0,0 @@
-data "cloudferro_kubernetes_version_v1" "version" {
-  version = "1.30.0"
-}
-
-data "cloudferro_kubernetes_version_v1" "version_by_id" {
-  id = "version id"
-}
-
--- examples/data-sources/cloudferro_machine_spec_v1/data-source.tf
+++
@@ -1,7 +0,0 @@
-data "cloudferro_machine_spec_v1" "spec" {
-  id = "spec id"
-}
-
-data "cloudferro_machine_spec_v1" "spec_by_name_and_region" {
-  name = "eo2.xlarge"
-}
