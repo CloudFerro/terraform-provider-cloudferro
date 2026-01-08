c048a9d18e5d9483b4d1e144d36850e4a7dc4cfd
Szymon Rams <srams@cloudferro.com>
2026-01-08 12:01:05 +0100

    update docs

--- docs/index.md
+++ docs/index.md
@@ -14,7 +14,6 @@ description: |-
 
 ```terraform
 provider "cloudferro" {
-  host        = "host"
   token       = "api token"
   server_cert = "path to the cert bundle"
   region      = "WAW4-1"
--- examples/resources/cloudferro_kubernetes_node_pool_v1/resource.tf
+++ examples/resources/cloudferro_kubernetes_node_pool_v1/resource.tf
@@ -1,8 +1,17 @@
 resource "cloudferro_kubernetes_node_pool_v1" "worker" {
-  name            = "worker node pool"
-  cluster_id      = "cluster id"
-  shared_networks = []
-  taints          = []
-  labels          = []
-  flavor          = "eo2a.2xlarge"
+  name       = "worker node pool"
+  cluster_id = "cluster id"
+  shared_networks = [
+    "shared network id",
+  ]
+  taints = [
+    { key = "key", value = "value", effect = "NoSchedule" },
+  ]
+  labels = [
+    { key = "key", value = "value" },
+  ]
+  flavor    = "eo2a.2xlarge"
+  autoscale = true
+  size_min  = 1
+  size_max  = 10
 }
